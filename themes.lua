fe0e642650fd77c987bc35fd240651d88b260293
v-rob <31123645+v-rob@users.noreply.github.com>
2017-12-03 17:49:51 -0800

    add red theme code

--- themes.lua
+++ themes.lua
@@ -3,12 +3,17 @@ laptop.themes = {
 		launcher_bg = "laptop_theme_blue_launcher_bg.png",
 		app_bg = "laptop_theme_blue_app_bg.png",
 		major_button = "laptop_theme_blue_major_button.png",
-		minor_button = "laptop_theme_blue_minor_button.png",
-		back_button = "", --TODO
-		exit_button = "", --TODO
+		minor_button = "laptop_theme_minor_button.png",
+		back_button = "laptop_theme_blue_back_button.png",
+		exit_button = "laptop_theme_blue_exit_button.png",
 	},
 	red = {
 		launcher_bg = "laptop_theme_red_launcher_bg.png",
+		app_bg = "laptop_theme_red_app_bg.png",
+		major_button = "laptop_theme_red_major_button.png",
+		minor_button = "laptop_theme_minor_button.png",
+		back_button = "laptop_theme_red_back_button.png",
+		exit_button = "laptop_theme_red_exit_button.png",
 	},
 	freedom = {
 		launcher_bg = "laptop_theme_freedom_launcher_bg.png",
