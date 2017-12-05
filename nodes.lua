08cc662f65b71f934b0f1ea6346d8a7a1203ccf8
Gerold55 <marleybeare56@gmail.com>
2017-12-04 21:00:17 -0500

    Add files via upload

--- nodes.lua
+++ nodes.lua
@@ -285,6 +285,104 @@ minetest.register_node("laptop:monitor2_off", {
 		}
 	}
 })
+
+minetest.register_node("laptop:monitor4_on", {
+    description = "Bell CrossOver",
+	tiles = {
+		"laptop_opti_pc_top.png^laptop_opti_kb_top.png^laptop_opti_ms_top.png^laptop_opti_lcb_top.png^laptop_opti_lcp_top.png^laptop_opti_lcd_top.png",
+		"laptop_opti_pc_bottom.png^laptop_opti_kb_bottom.png^laptop_opti_ms_bottom.png^laptop_opti_lcd_bottom.png",
+		"laptop_opti_pc_right.png^laptop_opti_kb_right.png^laptop_opti_ms_right.png^laptop_opti_lcb_right.png^laptop_opti_lcp_right.png^laptop_opti_lcd_right.png",
+		"laptop_opti_pc_left.png^laptop_opti_kb_left.png^laptop_opti_ms_left.png^laptop_opti_lcb_left.png^laptop_opti_lcp_left.png^laptop_opti_lcd_left.png",
+		"laptop_opti_pc_back.png^laptop_opti_kb_back.png^laptop_opti_ms_back.png^laptop_opti_lcb_back.png^laptop_opti_lcp_back.png^laptop_opti_lcd_back.png",
+		"laptop_opti_pc_on_front.png^laptop_opti_kb_front.png^laptop_opti_ms_front.png^laptop_opti_lcb_front.png^laptop_opti_lcp_front.png^laptop_opti_lcd_on_front.png",
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	drop = "laptop:monitor2_off",
+	groups = {choppy=2, oddly_breakably_by_hand=2, dig_immediate = 2, not_in_creative_inventory=1},
+	on_punch = function (pos, node, puncher)
+		local os = laptop.os_get(pos)
+		os:power_off("laptop:monitor4_off")
+	end,
+	on_construct = function(pos)
+		local os = laptop.os_get(pos)
+		os:set_theme("red")
+		os:power_on()
+		os:set_infotext('Bell CrossOver')
+	end,
+	after_place_node = laptop.after_place_node,
+	after_dig_node = laptop.after_dig_node,
+	stack_max = 1,
+	on_receive_fields = function(pos, formname, fields, sender)
+		local os = laptop.os_get(pos)
+		os:receive_fields(fields, sender)
+	end,
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.375, -0.5, -0.0625, 0.375, -0.3125, 0.4375}, -- tower
+			{-0.4375, -0.5, -0.4375, 0.25, -0.4375, -0.1875}, -- keboard
+			{0.3125, -0.5, -0.375, 0.4375, -0.4375, -0.1875}, -- mouse
+			{-0.25, -0.3125, 0.0625, 0.25, -0.25, 0.3125}, -- lcd_base
+			{-0.0625, -0.25, 0.1875, 0.0625, 0.0625, 0.25}, -- lcd_pedestal
+			{-0.4375, -0.125, 0.125, 0.4375, 0.4375, 0.1875}, -- lcd_main
+			
+		}
+	}
+})
+
+minetest.register_node("laptop:monitor4_off", {
+    description = "Bell CrossOver",
+	tiles = {
+		"laptop_opti_pc_top.png^laptop_opti_kb_top.png^laptop_opti_ms_top.png^laptop_opti_lcb_top.png^laptop_opti_lcp_top.png^laptop_opti_lcd_top.png",
+		"laptop_opti_pc_bottom.png^laptop_opti_kb_bottom.png^laptop_opti_ms_bottom.png^laptop_opti_lcd_bottom.png",
+		"laptop_opti_pc_right.png^laptop_opti_kb_right.png^laptop_opti_ms_right.png^laptop_opti_lcb_right.png^laptop_opti_lcp_right.png^laptop_opti_lcd_right.png",
+		"laptop_opti_pc_left.png^laptop_opti_kb_left.png^laptop_opti_ms_left.png^laptop_opti_lcb_left.png^laptop_opti_lcp_left.png^laptop_opti_lcd_left.png",
+		"laptop_opti_pc_back.png^laptop_opti_kb_back.png^laptop_opti_ms_back.png^laptop_opti_lcb_back.png^laptop_opti_lcp_back.png^laptop_opti_lcd_back.png",
+		"laptop_opti_pc_front.png^laptop_opti_kb_front.png^laptop_opti_ms_front.png^laptop_opti_lcb_front.png^laptop_opti_lcp_front.png^laptop_opti_lcd_front.png",
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	drop = "laptop:monitor4_off",
+	groups = {choppy=2, oddly_breakably_by_hand=2, dig_immediate = 2},
+	on_punch = function (pos, node, puncher)
+		local os = laptop.os_get(pos)
+		os:power_on("laptop:monitor4_on")
+	end,
+	after_place_node = laptop.after_place_node,
+	after_dig_node = laptop.after_dig_node,
+	stack_max = 1,
+	on_construct = function(pos)
+		local os = laptop.os_get(pos)
+		os:power_off()
+		os:set_infotext('Bell CrossOver')
+	end,
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.375, -0.5, -0.0625, 0.375, -0.3125, 0.4375}, -- tower
+			{-0.4375, -0.5, -0.4375, 0.25, -0.4375, -0.1875}, -- keboard
+			{0.3125, -0.5, -0.375, 0.4375, -0.4375, -0.1875}, -- mouse
+			{-0.25, -0.3125, 0.0625, 0.25, -0.25, 0.3125}, -- lcd_base
+			{-0.0625, -0.25, 0.1875, 0.0625, 0.0625, 0.25}, -- lcd_pedestal
+			{-0.4375, -0.125, 0.125, 0.4375, 0.4375, 0.1875}, -- lcd_main
+			
+		}
+	}
+})
+
+minetest.register_craft({
+	output = 'laptop:monitor4_off',
+	recipe = {
+		{'default:steel_ingot', 'default:steel_ingot', 'default:steel_ingot'},
+		{'default:steel_ingot', 'default:glass', 'default:steel_ingot'},
+		{'default:steel_ingot', 'default:gold_ingot', 'default:steel_ingot'},
+	}
+})
+
+--Old PC--
 minetest.register_node("laptop:monitor3_on", {
     description = "MT Desktop vintage 3.0",
 	tiles = {
