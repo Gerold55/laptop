eb9f7aeee9f8689fdaed7a462d35bdaaa7a5a7f2
v-rob <31123645+v-rob@users.noreply.github.com>
2017-12-06 16:21:03 -0800

    Add new theme code
    Themes are now in separate folders except for Freedom, which is the default.

---
+++ themes/basic_theme.lua
@@ -0,0 +1,9 @@
+laptop.register_theme("Basic", {
+	launcher_bg = "laptop_theme_basic_launcher_bg.png",
+	app_bg = "laptop_theme_basic_app_bg.png",
+	major_button = "laptop_theme_basic_button.png",
+	minor_button = "laptop_theme_basic_button.png",
+	back_button = "blank.png",
+	exit_button = "blank.png",
+	app_button = "blank.png",
+})
