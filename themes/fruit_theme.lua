02f6a7bcd40762b8da1953978a3a533e9d170e30
Gerold55 <marleybeare56@gmail.com>
2017-12-15 02:39:43 -0500

    Add files via upload

---
+++ themes/fruit_theme.lua
@@ -0,0 +1,9 @@
+laptop.register_theme("Fruit", {
+	launcher_bg = "laptop_theme_fruit_launcher_bg.png",
+	app_bg = "laptop_theme_fruit_app_bg.png",
+	major_button = "laptop_theme_fruit_major_button.png",
+	minor_button = "laptop_theme_minor_button.png",
+	back_button = "laptop_theme_fruit_back_button.png",
+	exit_button = "laptop_theme_fruit_exit_button.png",
+		app_button = "laptop_theme_fruit_app_button.png",
+})
