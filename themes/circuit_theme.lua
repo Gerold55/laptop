a99597c85b92760ccae310d5594ce7f602612c84
Grizzly Adam <35204361+Grizzly-Adam@users.noreply.github.com>
2018-02-15 17:30:28 -0600

    Four New Themes

---
+++ themes/circuit_theme.lua
@@ -0,0 +1,14 @@
+laptop.register_theme("Circuit", {
+	launcher_bg = "laptop_theme_circuit_launcher_bg.png",
+	app_bg = "laptop_theme_circuit_app_bg.png",
+	major_button = "laptop_theme_circuit_major_button.png",
+	back_button = "laptop_theme_circuit_back_button.png",
+	back_textcolor = "#000000",
+	exit_button = "laptop_theme_circuit_exit_button.png",
+	app_button = "blank.png",
+	textcolor = "#000000",
+	app_textcolor = "#ffffff",
+	taskbar_clock_position_and_size = "11,-.38;4,0.7",
+	exit_character = " ",
+	titlebar_textcolor = "#000000",
+})
---
+++ themes/snowpines_theme.lua
@@ -0,0 +1,14 @@
+laptop.register_theme("Snow Pines", {
+	launcher_bg = "laptop_theme_snowpines_launcher_bg.png",
+	app_bg = "laptop_theme_snowpines_app_bg.png",
+	major_button = "laptop_theme_snowpines_major_button.png",
+	minor_button = "laptop_theme_snowpines_major_button.png",
+	back_button = "laptop_theme_snowpines_exit_button.png",	
+	back_textcolor = "#000000",
+	exit_button = "laptop_theme_snowpines_exit_button.png",
+	exit_textcolor = "#000000",
+	app_button = "blank.png",
+	textcolor = "#ffffff",
+	app_textcolor = "#ffffff",
+	taskbar_clock_position_and_size = "5.5,-.31;4,0.7",
+})
