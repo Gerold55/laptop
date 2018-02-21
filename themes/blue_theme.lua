7e7dda3714f07dfe22f8e7f7471339298c9f5248
Grizzly Adam <35204361+Grizzly-Adam@users.noreply.github.com>
2018-02-21 10:44:42 -0600

    Add files via upload

--- themes/blue_theme.lua
+++ themes/blue_theme.lua
@@ -1,6 +1,6 @@
 laptop.register_theme("Blue", {
-	launcher_bg = "laptop_theme_blue_launcher_bg.png",
-	app_bg = "laptop_theme_blue_app_bg.png",
+	desktop_background = "laptop_theme_blue_desktop_background.png",
+	app_background = "laptop_theme_blue_app_background.png",
 	major_button = "laptop_theme_blue_major_button.png",
 	minor_button = "laptop_theme_minor_button.png",
 	back_button = "laptop_theme_blue_back_button.png",
