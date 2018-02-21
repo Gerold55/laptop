4a310cc2854417b5ee5d997b9a95394a42687ff8
Grizzly Adam <35204361+Grizzly-Adam@users.noreply.github.com>
2018-02-21 10:01:12 -0600

    Fix "tnt sweeper minor button" bug.

--- themes/boing_theme.lua
+++ themes/boing_theme.lua
@@ -3,7 +3,7 @@ laptop.register_theme("Boing!", {
 	app_bg = "laptop_theme_boing_app_bg.png",
 	major_button = "laptop_theme_boing_major_button.png",
 	major_textcolor = "#ffffff",
-	minor_button = "laptop_theme_snowpines_major_button.png",
+	minor_button = "laptop_theme_snowpines_minor_button.png",
 	minor_textcolor = "AAAAAA",
 	back_button = "laptop_theme_boing_back_button.png",	
 	back_textcolor = "#000000",
