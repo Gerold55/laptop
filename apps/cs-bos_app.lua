8b496d9c33f36b681ade83dc84721c51836f42ee
Alexander Weber <web.alexander@web.de>
2018-02-21 22:32:22 +0100

    get the app functional

--- apps/cs-bos_app.lua
+++ apps/cs-bos_app.lua
@@ -1,11 +1,23 @@
+local initial_message = {
+	"BASIC OPERATING SYSTEM",
+	"(C)COPYRIGHT 1982 CARDIFF-SOFT",
+	"128K RAM SYSTEM  77822 BYTES FREE",
+	"",
+}
+
+local function add_outline(outlines, line)
+	table.insert(outlines, line)
+	if #outlines > 25 then -- maximum lines count
+		table.remove(outlines,1)
+	end
+end
+
+
 laptop.register_app("cs-bos_launcher", {
 	app_name = "CS-BOS v3.31",
 	fullscreen = true,
 
 	formspec_func = function(cs_bos, mtos)
-	local screenLine0 = "BASIC OPERATING SYSTEM\n(C)COPYRIGHT 1982 CARDIFF-SOFT\n128K RAM SYSTEM  77822 BYTES FREE"
-	local screenLine9 = '9'
-	local screenPrompt = '> '
 
 		-- no system found. Error
 		if not mtos.sysdata then
@@ -26,24 +38,30 @@ laptop.register_app("cs-bos_launcher", {
 		end
 
 		local data = mtos.bdev:get_app_storage('ram', 'cs_bos')
-		data.outlines = data.outlines or {}
+
+		data.outlines = data.outlines or table.copy(initial_message)
 		data.inputfield = data.inputfield or ""
 
 		local formspec = 
-			"size[15,10]"..
-			"field[0.031,9.93;14.18,1;data.enter;;"..screenPrompt..screenLine9..data.inputfield.."]"..
-			"textlist[-.35,-.35;15.57, 10.12;crtDisplay;list=\n \n \n"..screenLine0.."\n"..screenLine9.."\n]"..
-			"button[13.75,9.85;1.5,0.55;enter;Enter]"
+				"size[15,10]"..
+				"field[0.031,9.93;14.18,1;inputfield;;"..minetest.formspec_escape(data.inputfield).."]"..
+				"textlist[-.35,-.35;15.57, 10.12;outlines;"
+		for idx,line in ipairs(data.outlines) do
+			if idx > 1 then
+				formspec = formspec..','
+			end
+			formspec = formspec..minetest.formspec_escape(line)
+		end
+		formspec = formspec..";"..#data.outlines.."]"..
+				"button[13.75,9.85;1.5,0.55;button_enter;Enter]"..
+				"field_close_on_enter[inputfield;false]"
 		return formspec
 	end,
 
-
-
-
-
-	receive_fields_func = function(app, mtos, sender, fields)
+	receive_fields_func = function(cs_bos, mtos, sender, fields)
 		local data = mtos.bdev:get_app_storage('ram', 'cs_bos')
-		data.outlines = data.outlines or {}
+
+		data.outlines = data.outlines or table.copy(initial_message)
 		data.inputfield = data.inputfield or ""
 
 		if fields.inputfield then -- move received data to the formspec input field
@@ -52,8 +70,21 @@ laptop.register_app("cs-bos_launcher", {
 
 		if fields.key_enter or fields.button_enter then
 		-- run the command
-
+			local exec_all = data.inputfield:split(" ")
+			local exec_command = exec_all[1] --further parameters are 2++
+			add_outline(data.outlines, "> "..data.inputfield)
 			data.inputfield = ""
+			if exec_command == nil then --empty line
+			elseif exec_command == "cls" then
+				data.outlines = nil -- reset screen
+			elseif exec_command == "exit" then
+				data.outlines = nil  -- reset screen
+				mtos:set_app()  -- exit app (if in app mode)
+			elseif exec_command == "date" then
+				add_outline(data.outlines, os.date())
+			else
+				add_outline(data.outlines, exec_command..": command not found")
+			end
 		end
 	end,
 
