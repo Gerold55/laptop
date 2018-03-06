606dba1a644ced0322ab403995c98a93f8a451c5
Alexander Weber <web.alexander@web.de>
2018-03-06 13:56:02 +0100

    renamed laptop_hard_drive.png to laptop_removable.png

--- apps/removable_app.lua
+++ apps/removable_app.lua
@@ -1,6 +1,6 @@
 laptop.register_app("removable", {
 	app_name = "Removable Storage",
-	app_icon = "laptop_hard_drive.png",
+	app_icon = "laptop_removable.png",
 	app_info = "Interface with Removable Media",
 	formspec_func = function(app, mtos)
 		local formspec = 
