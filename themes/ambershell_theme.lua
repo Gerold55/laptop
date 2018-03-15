5661c14a4e1f295a844a131544f5d9b4ad14ef9c
Grizzly Adam <35204361+Grizzly-Adam@users.noreply.github.com>
2018-03-15 18:37:30 -0500

    Added status colors

--- themes/ambershell_theme.lua
+++ themes/ambershell_theme.lua
@@ -21,10 +21,12 @@ laptop.register_theme("Amber Shell", {
 	table_highlight_bgcolor = '#a77200',
 	table_highlight_textcolor = '#000000',
 	muted_textcolor = "#6b4900",
+	status_online_textcolor = "#000000",
+	status_disabled_textcolor = "#a77200",
+	status_off_textcolor = "#000000",
 	os_max_version = '3.99',
 	texture_replacements = {
-		['laptop_mail.png'] = 'laptop_theme_ambershell_email_letter_new.png', --make new launcher icon
-		['laptop_email_letter.png'] = 'laptop_theme_ambershell_mail.png',
+		['laptop_mail.png'] = 'laptop_theme_ambershell_email_letter_new.png',
 		['laptop_mail_received.png'] = 'laptop_theme_ambershell_mail_received.png',
 		['laptop_mail_sentbox.png'] = 'laptop_theme_ambershell_mail_sentbox.png',
 		['laptop_email_new.png'] = 'laptop_theme_ambershell_email_new.png',
@@ -35,5 +37,16 @@ laptop.register_theme("Amber Shell", {
 		['laptop_mail_button.png'] = 'laptop_theme_ambershell_mail_button.png',
 		['printer_button.png'] = 'laptop_theme_ambershell_printer_button.png',
 		['laptop_boom.png'] = 'laptop_theme_ambershell_laptop_boom.png',
+		--shell icons
+		['laptop_email_letter.png'] = 'laptop_theme_ambershell_mail_icon.png',
+		['laptop_removable.png'] = 'laptop_theme_ambershell_removable_icon.png',
+		['laptop_tetris_icon.png'] = 'laptop_theme_ambershell_tetris_icon.png',
+		['laptop_cs_bos.png'] = 'laptop_theme_ambershell_cs_bos_icon.png',
+		['laptop_tnt.png'] = 'laptop_theme_ambershell_tnt_icon.png',
+		['laptop_realchess_chessboard_icon.png'] = 'laptop_theme_ambershell_realchess_chessboard_icon.png',
+		['laptop_calculator.png'] = 'laptop_theme_ambershell_calculator_icon.png',
+		['laptop_notes_pad.png'] = 'laptop_theme_ambershell_notepad_icon.png',
+		--chess
+		['laptop_realchess_chess_bg.png'] = 'laptop_theme_ambershell_realchess_chess_bg.png',
 	}
 })
